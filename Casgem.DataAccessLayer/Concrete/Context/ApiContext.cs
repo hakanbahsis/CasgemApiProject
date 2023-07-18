3ad0f98776e64c9fae354b538b80421078de1259
hakanbahsis <hakan.bahsis@hotmail.com>
2023-07-18 14:28:20 +0300

    Add project files.

---
+++ Casgem.DataAccessLayer/Concrete/Context/ApiContext.cs
@@ -0,0 +1,22 @@
+﻿using Casgem.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem.DataAccessLayer.Concrete.Context
+{
+    public class ApiContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("data source=DESKTOP-PK98KLS;initial catalog=CasgemApiDb;integrated security=true;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Casgem.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using Casgem.DataAccessLayer.Abstract;
+using Casgem.DataAccessLayer.Concrete.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        private readonly ApiContext _context;
+
+        public GenericRepository(ApiContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
